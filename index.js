537f8506e00651940c940ceed189a7239d18531c
Greg Allen <me@jga.me>
2015-10-26 15:31:07 -0700

    expose a load function for plugins to use

--- index.js
+++ index.js
@@ -10,54 +10,67 @@ var defaults = {
 };
 exports.register = function(server, options, next) {
 
-  var settings = _.clone(options);
-  _.defaults(settings, defaults);
+  var load = function(options, done) {
+    done = done || function() {};
+    var settings = _.clone(options);
+    _.defaults(settings, defaults);
 
-  fs.stat(settings.path, function(err, stat) {
-
-    if (err) {
-      return next(err);
-    }
-    if (!stat.isDirectory()) {
-      return next(new Error(settings.path + ' is not a directory'));
-    }
-
-    glob('**/*.js', {
-      cwd: settings.path
-    }, function(err, files) {
-      if (err) {
-        return next(err);
+    fs.exists(settings.path, function(exists) {
+      if (!exists) {
+        if (settings.verbose) {
+          server.log(['hapi-route-loader', 'debug'], { message: 'route directory doesn\'t exist', path: settings.path });
+        }
+        return next();
       }
+      fs.stat(settings.path, function(err, stat) {
 
-      files.forEach(function(file) {
-        var segment = file.replace(settings.path, '').split(path.sep);
-        segment.pop();
-        segment = segment.join('/');
+        if (err) {
+          return done(err);
+        }
+        if (!stat.isDirectory()) {
+          return done(new Error(settings.path + ' is not a directory'));
+        }
 
-        var routeObj = require(path.join(settings.path, file));
-
-        _.forIn(routeObj, function(route) {
-          var tmpPath = route.path || '';
-
-          route.path = _.trimRight(settings.base, '/');
-
-          if (_.startsWith(tmpPath, '/')) {
-            route.path += tmpPath;
-          } else {
-            route.path += '/' + segment + '/' + _.trimLeft(tmpPath, '/');
+        glob('**/*.js', {
+          cwd: settings.path
+        }, function(err, files) {
+          if (err) {
+            return done(err);
           }
 
-          if (settings.verbose) {
-            server.log(['hapi-route-loader', 'debug'], { message: 'route loaded', route: route });
-          }
-          server.route(route);
+          files.forEach(function(file) {
+            var segment = file.replace(settings.path, '').split(path.sep);
+            segment.pop();
+            segment = segment.join('/');
+
+            var routeObj = require(path.join(settings.path, file));
+
+            _.forIn(routeObj, function(route) {
+              var tmpPath = route.path || '';
+
+              route.path = _.trimRight(settings.base, '/');
+
+              if (_.startsWith(tmpPath, '/')) {
+                route.path += tmpPath;
+              } else {
+                route.path += '/' + segment + '/' + _.trimLeft(tmpPath, '/');
+              }
+
+              if (settings.verbose) {
+                server.log(['hapi-route-loader', 'debug'], { message: 'route loaded', route: route });
+              }
+              server.route(route);
+            });
+          });
+
+          done();
         });
       });
-
-      next();
     });
+  };
 
-  });
+  server.expose('load', load);
+  load(options, next);
 
 };
 
