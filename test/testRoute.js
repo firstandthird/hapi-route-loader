adf41e302b85eb16a80f852678068745dc7be2d2
chris <orthagonal1@gmail.com>
2015-11-19 15:57:17 -0600

    added hapi to devDependencies supports 'npm test' run from any directory

--- test/testRoute.js
+++ test/testRoute.js
@@ -6,7 +6,7 @@ let Hapi = require('hapi');
 let request = require('request');
 
 let launchServer = function(server, port, options, done){
-  options.path= process.cwd() + "/test/routes";
+  options.path= __dirname + "/routes";
   server.connection({ port: port });
   server.register([{
     register: require('../'),
