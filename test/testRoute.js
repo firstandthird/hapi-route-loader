1200df3d5a37e5b70a9b3bae7cdfeffe9a58ba46
chris <orthagonal1@gmail.com>
2016-05-11 21:47:09 -0500

    es6, linted

--- test/testRoute.js
+++ test/testRoute.js
@@ -1,101 +1,100 @@
-'use strict'
-let chai = require('chai');
-let assert = chai.assert;
-let should = chai.should();
-let Hapi = require('hapi');
-let request = require('request');
+'use strict';
+const chai = require('chai');
+const assert = chai.assert;
+const Hapi = require('hapi');
+const request = require('request');
+const mod = require('../index.js');
 
-let launchServer = function(server, port, options, done){
-  options.path= __dirname + "/routes";
-  server.connection({ port: port });
+const launchServer = (server, port, options, done) => {
+  options.path = `${__dirname}/routes`;
+  server.connection({ port });
   server.register([{
-    register: require('../'),
-    options :  options,
+    register: mod,
+    options
   }
-  ], function (err) {
+], (err) => {
     if (err) {
       console.error('Failed to load plugin:', err);
       return done(err);
     }
-    server.start(function(err){
-      if (err){
-        console.log(err)
-        return done(err);
+    server.start((startErr) => {
+      if (startErr) {
+        console.log(startErr);
+        return done(startErr);
       }
       done();
     });
   });
-}
+};
 
-
-describe('hapi-route-loader /dashboard base', function(done){
-  let server = new Hapi.Server();
-  let port = 8080;
-  let options = {base:'/dashboard'};
-  before(function(done){
-    launchServer(server,port,options,done);
+describe('hapi-route-loader /dashboard base', () => {
+  const server = new Hapi.Server();
+  const port = 8080;
+  const options = { base: '/dashboard' };
+  before((beforeDone) => {
+    launchServer(server, port, options, beforeDone);
   });
-  it("base: '/dashboard', path: 'get' => '/dashboard/get'", function(done){
-    request.get('http://localhost:8080/dashboard/get', function(err,response){
-      assert(response.body=="/get", 'dashboard/get works');
+  it('base: "/dashboard", path: "get" => "/dashboard/get"', (done) => {
+    request.get('http://localhost:8080/dashboard/get', (err, response) => {
+      assert(response.body === '/get', 'dashboard/get works');
       done();
     });
   });
-  it("base: '/dashboard', path: null => '/dashboard'", function(done){
-    request.get('http://localhost:8080/dashboard', function(err,response){
-      assert(response.body=="/", '/dashboard as base');
+  it('base: "/dashboard", path: null => "/dashboard"', (done) => {
+    request.get('http://localhost:8080/dashboard', (err, response) => {
+      assert(response.body === '/', '/dashboard as base');
       done();
     });
   });
-  it("base: '/dashboard', path: '{id}' => '/dashboard/{id}'", function(done){
-    request.get('http://localhost:8080/dashboard/testId', function(err,response){
-      assert(response.body=="testId", 'param passed correctly');
+  it('base: "/dashboard", path: "{id}" => "/dashboard/{id}"', (done) => {
+    request.get('http://localhost:8080/dashboard/testId', (err, response) => {
+      assert(response.body === 'testId', 'param passed correctly');
       done();
     });
   });
 });
 
-describe('hapi-route-loader / base', function(done){
-  let server = new Hapi.Server();
-  let port = 8081;
-  let options = {base:'/'};
-  before(function(done){
-    launchServer(server,port,options,function(){
+describe('hapi-route-loader / base', () => {
+  const server = new Hapi.Server();
+  const port = 8081;
+  const options = { base: '/' };
+  before((done) => {
+    launchServer(server, port, options, () => {
       server.route({
         method: 'GET',
         path: '/dashboard',
-        handler: function (request, reply) {
-          reply('/dashboard')
+        handler: (req, reply) => {
+          reply('/dashboard');
         }
       });
       done();
     });
   });
-  it(" base: '/', path: '/dashboard' => '/dashboard'", function(done){
-    request.get('http://localhost:8081/dashboard', function(err,response){
-      assert(response.body=="/dashboard", '/dashboard as base');
+  it(' base: "/", path: "/dashboard" => "/dashboard"', (done) => {
+    request.get('http://localhost:8081/dashboard', (err, response) => {
+      assert(response.body === '/dashboard', '/dashboard as base');
       done();
     });
   });
 });
-describe('hapi-route-loader /dashboard/ base', function(done){
-  let server = new Hapi.Server();
-  let port = 8082;
-  let options = {base:'/dashboard/'};
-  before(function(done){
-    launchServer(server,port,options,function(){
+describe('hapi-route-loader /dashboard/ base', () => {
+  const server = new Hapi.Server();
+  const port = 8082;
+  const options = { base: '/dashboard/' };
+  before((done) => {
+    launchServer(server, port, options, () => {
       done();
     });
   });
-  it("base: '/dashboard/', path: null => '/dashboard/'", function(done){
-    request.get('http://localhost:8082/dashboard/', function(err,response){
-      assert(response.body=="/", '/dashboard/ works');
+  it('base: "/dashboard/", path: null => "/dashboard/"', (done) => {
+    request.get('http://localhost:8082/dashboard/', (err, response) => {
+      assert(response.body === '/', '/dashboard/ works');
       done();
     });
   });
-  it("base: '/dashboard/', path: '/user' => '/user'", function(done){
-    request.get('http://localhost:8082/user', function(err,response){
-      assert(response.body=="/user", 'user works');
+  it('base: "/dashboard/", path: "/user" => "/user"', (done) => {
+    request.get('http://localhost:8082/user', (err, response) => {
+      assert(response.body === '/user', 'user works');
       done();
     });
   });
