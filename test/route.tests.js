9663acc2f214e4b77f894e7c00647559430f5680
Chris <orthagonal1@gmail.com>
2016-10-14 13:21:59 -0500

    fixed up tests to be clearer

---
+++ test/route.tests.js
@@ -0,0 +1,209 @@
+'use strict';
+const chai = require('chai');
+const assert = chai.assert;
+const describe = require('mocha').describe;
+const afterEach = require('mocha').afterEach;
+const beforeEach = require('mocha').beforeEach;
+const it = require('mocha').it;
+const request = require('request');
+const Hapi = require('hapi');
+const routeLoader = require('../');
+
+const setupServerPlugin = (options, routes, callback) => {
+  const server = new Hapi.Server({});
+  server.connection({ port: 8080 });
+  server.register({
+    register: routeLoader,
+    options
+  }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    server.start((err2) => {
+      if (err2) {
+        console.log(err2);
+      }
+      routes.forEach((route) => {
+        server.route(route);
+      });
+      callback(server);
+    });
+  });
+};
+
+describe('hapi-route-loader /dashboard base', () => {
+  let server;
+  const options = {
+    path: `${__dirname}/routes`,
+    base: '/dashboard'
+  };
+  beforeEach((done) => {
+    setupServerPlugin(options, [], (returnedServer) => {
+      server = returnedServer;
+      done();
+    });
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+  it("base: '/dashboard', path: 'get' => '/dashboard/get'", (done) => {
+    request.get('http://localhost:8080/dashboard/get', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/get', 'dashboard/get works');
+      done();
+    });
+  });
+  it("base: '/dashboard', path: null => '/dashboard'", (done) => {
+    request.get('http://localhost:8080/dashboard', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/', '/dashboard as base');
+      done();
+    });
+  });
+  it("base: '/dashboard', path: '{id}' => '/dashboard/{id}'", (done) => {
+    request.get('http://localhost:8080/dashboard/testId', (err, response) => {
+      assert(err === null);
+      assert(response.body === 'testId', 'param passed correctly');
+      done();
+    });
+  });
+});
+
+describe('hapi-route-loader / base', () => {
+  let server;
+  const options = {
+    base: '/',
+    path: `${__dirname}/routes`
+  };
+  beforeEach((done) => {
+    setupServerPlugin(options, [{
+      method: 'GET',
+      path: '/dashboard',
+      handler: (launchRequest, reply) => {
+        reply('/dashboard');
+      }
+    }], (returnedServer) => {
+      server = returnedServer;
+      done();
+    });
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+  it(" base: '/', path: '/dashboard' => '/dashboard'", (done) => {
+    request.get('http://localhost:8080/dashboard', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/dashboard', '/dashboard as base');
+      done();
+    });
+  });
+  it('will preserve the config: pre: [] option for routes', (done) => {
+    request.post('http://localhost:8080/', {}, (err, response) => {
+      assert(err === null);
+      assert(response.body === 'preProcessed', 'config.pre is preserved');
+      done();
+    });
+  });
+});
+
+describe('hapi-route-loader /dashboard/ base', () => {
+  let server;
+  const options = {
+    path: `${__dirname}/routes`,
+    base: '/dashboard/'
+  };
+  beforeEach((done) => {
+    setupServerPlugin(options, [], (returnedServer) => {
+      server = returnedServer;
+      done();
+    });
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+  it("base: '/dashboard/', path: null => '/dashboard/'", (done) => {
+    request.get('http://localhost:8080/dashboard/', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/', '/dashboard/ works');
+      done();
+    });
+  });
+  it("base: '/dashboard/', path: '/user' => '/user'", (done) => {
+    request.get('http://localhost:8080/user', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/user', 'user works');
+      done();
+    });
+  });
+});
+
+describe('hapi-route-loader.routeLoader function will also load routes', () => {
+  let server;
+  const options = {
+    path: `${__dirname}/routes`,
+    base: '/dashboard'
+  };
+  beforeEach((done) => {
+    server = new Hapi.Server({});
+    server.connection({ port: 8080 });
+    routeLoader.routeLoader(server, options, (err) => {
+      if (err) {
+        return done(err);
+      }
+      server.start((err2) => {
+        if (err2) {
+          console.log(err2);
+        }
+        done(err, server);
+      });
+    });
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+
+  it('works as an exported module method, base: /dashboard, path: get => /dashboard/get', (done) => {
+    request.get('http://localhost:8080/dashboard/get', (err, response) => {
+      assert(err === null);
+      assert(response.body === '/get', 'dashboard/get works');
+      done();
+    });
+  });
+});
+
+describe('hapi-route-loader lets you specify routeConfig object for all routes', () => {
+  let server;
+  // this will be assigned to all routes
+  const pre1 = (request, reply) => {
+    return reply('global!');
+  };
+  const options = {
+    path: `${__dirname}/globalConfigRoutes`,
+    base: '/dashboard',
+    routeConfig: {
+      pre: [
+         { method: pre1, assign: 'm1' },
+      ]
+    }
+  };
+
+  beforeEach((done) => {
+    setupServerPlugin(options, [], (returnedServer) => {
+      server = returnedServer;
+      done();
+    });
+  });
+  afterEach((done) => {
+    server.stop(done);
+  });
+  it('base: /dashboard, path: get => /dashboard/get', (done) => {
+    request.get('http://localhost:8080/dashboard/get', (err, response) => {
+      assert(err === null);
+      assert(response.body === 'global!');
+      done();
+    });
+  });
+});
